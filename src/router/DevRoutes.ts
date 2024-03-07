949471e4d0ed0760dc6e7d2025c42ad135fd80ba
Link <xieaolin@gmail.com>
2024-03-07 21:27:31 +0800

    feat: add a quick wallet developing page

--- src/router/DevRoutes.ts
+++ src/router/DevRoutes.ts
@@ -2,6 +2,11 @@ const DevRoutes = {
     path: '/dev',
     component: () => import('@/layouts/MainLayout.vue'),
     children: [
+      {
+        name: 'WalletDev',
+        path: '/dev/wallet',
+        component: () => import('@/views/dev/WalletDev.vue')
+      },
     ]
 };
 
